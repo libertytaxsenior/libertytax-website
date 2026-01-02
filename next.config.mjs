680227fde5e9180e30bff86f8b4a70a2cc75ebd7
libertyhealth <melissa@friersoncfo.com>
2026-01-01 19:14:22 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,7 @@
-export default { poweredByHeader: false };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+}
+
+export default nextConfig
